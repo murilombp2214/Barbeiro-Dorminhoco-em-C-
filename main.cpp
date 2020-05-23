c867667613a2c28186b63bd0c82c854dd01e5f09
Murilo Barros Peixoto <38633004+murilombp2214@users.noreply.github.com>
2020-05-23 12:00:43 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,19 @@
 #include<semaphore.h>
 #include <unistd.h> 
 #include <pthread.h>    
+
+#include <sys/mman.h>
+#include<stddef.h>
+
+#include <sys/ipc.h>
+#include <sys/shm.h>
 using namespace std;
 
-
 static sem_t objSemafaro; //objeto semafaro
-
-int countSemafaro = 3; //quantidade de maxima no semafao
-int qtdCliente = 0;
-
+static int shm_id;
+static key_t key = 1541;
+static int countSemafaro; //quantidade de maxima no semafao
+static int* qtdCliente;
 class Semafaro{
     public:
         //cria um semafaro
@@ -22,16 +27,22 @@ class Semafaro{
 };
 
 
+void create_shared_memory() {
+     shm_id = shmget(key, 10*sizeof(int), 0666 | IPC_CREAT);
+     qtdCliente = (int*)shmat(shm_id, NULL, 0);
+     qtdCliente[0] = 0;
+}
+
 
 void iniciarBarbeiro(){
     while(true){
         sleep(5);
-        cout << qtdCliente << endl;
-        if(qtdCliente > 0){
+        cout << qtdCliente[0] << endl;
+        if(qtdCliente[0] > 0){
             cout << "Cortando cabelo....." << endl;
             sem_post(&objSemafaro);
             cout << "Indo embora....." << endl;
-            qtdCliente--;
+            qtdCliente[0]--;
         }else{
             cout << "O barbeiro esta dormindo....." << endl;
         }
@@ -40,18 +51,19 @@ void iniciarBarbeiro(){
 
 
 void criarCliente(string nome){
-    cout << "Atual " <<  qtdCliente << endl;
-    if(countSemafaro == qtdCliente){
+    cout << "Atual " <<  qtdCliente[0] << endl;
+    if(countSemafaro == qtdCliente[0]){
         cout << "O Cliente "+nome+" foi embora pois não havia lugar para esperar...." << endl;     
     }else{
         cout << "Aguardando para corte de cabelo....." << endl;
-        qtdCliente++; //atualiza a quantidqade de cliente
+        qtdCliente[0]++; //atualiza a quantidqade de cliente
         sem_wait(&objSemafaro); //poem cliente no semafarto
     }
 
 }
 
 int main(){
+    create_shared_memory();
     Semafaro::criar(countSemafaro,objSemafaro);
     int pid = fork(); 
     if (pid == 0)  //iniciar o barbeito em um processo
@@ -66,10 +78,9 @@ int main(){
 
             if(fork() == 0){ //se o processo for o processo filho cria o cliente
                 criarCliente(cliente);
+                qtdCliente[0]++;
                 break;
             }
-            qtdCliente++;
-
         }
     }
     
